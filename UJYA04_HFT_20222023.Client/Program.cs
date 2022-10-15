e2651070eee32a92c618af0bc6a1b0ce1fb9a98f
incarrnati0n <horvathimi2001@gmail.com>
2022-10-15 14:42:14 +0200

    Add project files.

---
+++ UJYA04_HFT_20222023.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UJYA04_HFT_20222023.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
