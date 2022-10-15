45ae804343b9b8dc74b5967294c9ece7989c5bf0
incarrnati0n <horvathimi2001@gmail.com>
2022-10-15 15:30:33 +0200

    DbContext class created

--- UJYA04_HFT_20222023.Repository/FootyDbContext.cs
+++ UJYA04_HFT_20222023.Repository/FootyDbContext.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using UJYA04_HFT_20222023.Models;
 
 namespace UJYA04_HFT_20222023.Repository
 {
     public class FootyDbContext : DbContext
     {
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<Players> Players { get; set; }
+        public DbSet<Managers> Managers { get; set; }
+
+        public FootyDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C: \Users\imcon\Desktop\Egyetem folder\HFT\UJYA04_HFT_20222023\UJYA04_HFT_20222023.Repository\FootyDb.mdf;Integrated Security = True;MultipleActiveResultSets = True";
+
+                optionsBuilder
+                .UseSqlServer(conn);
+            }
+
+            
+        }
+
 
     }
 }
