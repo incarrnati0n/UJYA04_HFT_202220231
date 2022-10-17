763b2398f74eec5da92e5d474802cc0b523b030e
incarrnati0n <horvathimi2001@gmail.com>
2022-10-17 16:30:10 +0200

    ManagerLogic and IManagerLogic created

--- UJYA04_HFT_20222023.Logic/LogicClasses/ManagersLogic.cs
+++ UJYA04_HFT_20222023.Logic/LogicClasses/ManagersLogic.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UJYA04_HFT_20222023.Models;
+using UJYA04_HFT_20222023.Repository;
 
 namespace UJYA04_HFT_20222023.Logic.LogicClasses
 {
-    internal class ManagersLogic
+    internal class ManagersLogic : IManagersLogic
     {
+        IRepository<Managers> repo;
+
+        //CRUD methods
+
+
+        public ManagersLogic(IRepository<Managers> repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public void Create(Managers item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Managers Read(int id)
+        {
+            var managers = Read(id);
+            if (managers == null)
+            {
+                throw new ArgumentException("This manager doesn't exist!");
+            }
+            return managers;
+        }
+
+        public IQueryable<Managers> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Managers item)
+        {
+            this.repo.Update(item);
+        }
+
+        //NON-CRUD methods
+
+        public double GetAverageSalary()
+        {
+            return this.repo
+                .ReadAll()
+                .Average(m => m.ManagerSalary);
+        }
+
+
+
     }
 }
