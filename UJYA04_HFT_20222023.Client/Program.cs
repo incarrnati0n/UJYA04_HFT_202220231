d245bb91bdd1be1f2bd4efe2d944d4464d94cf09
incarrnati0n <horvathimi2001@gmail.com>
2022-10-26 10:56:21 +0200

    submenus added to Main function with the RestService entity

--- UJYA04_HFT_20222023.Client/Program.cs
+++ UJYA04_HFT_20222023.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using UJYA04_HFT_20222023.Repository;
 using UJYA04_HFT_20222023.Models;
 using System.Collections.Generic;
+using ConsoleTools;
 
 namespace UJYA04_HFT_20222023.Client
 {
@@ -68,31 +69,31 @@ namespace UJYA04_HFT_20222023.Client
             {
                 Console.Write("Enter Player's id to update: ");
                 int id = int.Parse(Console.ReadLine());
-                Players one = rest.Get<Players>(id, "actor");
+                Players one = rest.Get<Players>(id, "player");
                 Console.Write($"New name [old: {one.PlayerName}]: ");
                 string name = Console.ReadLine();
                 one.PlayerName = name;
-                rest.Put(one, "actor");
+                rest.Put(one, "player");
             }
             else if (entity == "Manager")
             {
                 Console.Write("Enter Manager's id to update: ");
                 int id = int.Parse(Console.ReadLine());
-                Managers one = rest.Get<Managers>(id, "actor");
+                Managers one = rest.Get<Managers>(id, "manager");
                 Console.Write($"New name [old: {one.ManagerName}]: ");
                 string name = Console.ReadLine();
                 one.ManagerName = name;
-                rest.Put(one, "actor");
+                rest.Put(one, "manager");
             }
             else
             {
-                Console.Write("Enter Manager's id to update: ");
+                Console.Write("Enter Team's id to update: ");
                 int id = int.Parse(Console.ReadLine());
-                Teams one = rest.Get<Teams>(id, "actor");
+                Teams one = rest.Get<Teams>(id, "team");
                 Console.Write($"New name [old: {one.TeamName}]: ");
                 string name = Console.ReadLine();
                 one.TeamName = name;
-                rest.Put(one, "actor");
+                rest.Put(one, "team");
             }
         }
 
@@ -120,27 +121,37 @@ namespace UJYA04_HFT_20222023.Client
 
         static void Main(string[] args)
         {
-            //FootyDbContext ctx = new FootyDbContext();
 
-            //var teams = ctx.Teams.ToList();
-            //foreach (var team in teams)
-            //{
-            //    Console.WriteLine($"Team Name: {team.TeamName}, Stadium: {team.TeamStadiumName}");
-            //}
-            //var players = ctx.Players.ToList();
-            //foreach (var player in players)
-            //{
-            //    Console.WriteLine($"Player Name: {player.PlayerName}, Rating: {player.Rating}");
-            //}
-            //var managers = ctx.Managers.ToList();
-            //foreach (var manager in managers)
-            //{
-            //    Console.WriteLine($"Manager name: {manager.ManagerName}, Salary: {manager.ManagerSalary}");
-            //}
+            rest = new RestService("http://localhost:65535", "team");
 
+            var playerSubMenu = new ConsoleMenu(args, level: 1)
+               .Add("List", () => List("Player"))
+               .Add("Create", () => Create("Player"))
+               .Add("Delete", () => Delete("Player"))
+               .Add("Update", () => Update("Player"))
+               .Add("Exit", ConsoleMenu.Close);
 
+            var managerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Manager"))
+                .Add("Create", () => Create("Manager"))
+                .Add("Delete", () => Delete("Manager"))
+                .Add("Update", () => Update("Manager"))
+                .Add("Exit", ConsoleMenu.Close);
 
+            var teamSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Team"))
+                .Add("Create", () => Create("Team"))
+                .Add("Delete", () => Delete("Team"))
+                .Add("Update", () => Update("Team"))
+                .Add("Exit", ConsoleMenu.Close);
 
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Player", () => playerSubMenu.Show())
+                .Add("Manager", () => managerSubMenu.Show())
+                .Add("Team", () => teamSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
 
         }
     }
