33ea4de3fc4aba0f4728c2d3e4f8b087760da5d8
incarrnati0n <horvathimi2001@gmail.com>
2023-03-14 11:45:09 +0100

    TeamsViewModel && TeamsWindow

--- UJYA04_HFT_20222023/ViewModels/TeamsViewModel.cs
+++ UJYA04_HFT_20222023/ViewModels/TeamsViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using UJYA04_HFT_20222023.Models;
 
-namespace UJYA04_HFT_20222023.WpfClient.ViewModels
+namespace UJYA04_HFT_20222023.WpfClient
 {
     public class TeamsViewModel : ObservableRecipient
     {
@@ -14,19 +18,67 @@ namespace UJYA04_HFT_20222023.WpfClient.ViewModels
 
         private Teams selectedTeam;
 
-        public Teams MyProperty
+        public Teams SelectedTeam
         {
             get { return selectedTeam; }
             set 
             {
-                selectedTeam = new Teams()
+                if (value != null)
                 {
-                    TeamName = value.TeamName,
-                    TeamId = value.TeamId,
-                    TeamFoundedYear = value.TeamFoundedYear,
-                    TeamOwner = value.TeamOwner,
-                    TeamStadiumName = value.TeamStadiumName,
-                };
+                    selectedTeam = new Teams()
+                    {
+                        TeamName = value.TeamName,
+                        TeamId = value.TeamId,
+                    };
+                }
+                OnPropertyChanged();
+                (DeleteTeamCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+            
+        }
+
+        public ICommand CreateTeamCommand { get; set; }
+        public ICommand DeleteTeamCommand { get; set; }
+        public ICommand UpdateTeamCommand { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public TeamsViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Teams = new RestCollection<Teams>("http://localhost:24518/", "Teams", "hub");
+
+                CreateTeamCommand = new RelayCommand(() =>
+                {
+                    Teams.Add(new Teams()
+                    {
+                        TeamName = SelectedTeam.TeamName
+                    });
+                });
+
+                DeleteTeamCommand = new RelayCommand(() =>
+                {
+                    Teams.Delete(SelectedTeam.TeamId);
+                },
+                () =>
+                {
+                    return SelectedTeam != null;
+                }
+                );
+                UpdateTeamCommand = new RelayCommand(() =>
+                {
+                    Teams.Update(SelectedTeam);
+                });
+
+                SelectedTeam = new Teams();
             }
         }
 
