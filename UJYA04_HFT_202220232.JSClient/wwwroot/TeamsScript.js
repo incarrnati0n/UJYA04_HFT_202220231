f041746ac88fe9291a45529338b90280dc4094ff
incarrnati0n <horvathimi2001@gmail.com>
2023-03-17 17:04:30 +0100

    fixed a bug with create method and added delete method

--- UJYA04_HFT_202220232.JSClient/wwwroot/TeamsScript.js
+++ UJYA04_HFT_202220232.JSClient/wwwroot/TeamsScript.js
@@ -3,7 +3,7 @@
 getdata();
 
 async function getdata() {
-    await fetch('http://localhost:24518/Teams')
+    await fetch('http://localhost:24518/Teams/')
           .then(x => x.json())
           .then(y => {
             teams = y;
@@ -17,24 +17,38 @@ async function getdata() {
 
 
 function display() {
+    document.getElementById('databoard').innerHTML = "";
     teams.forEach(t => {
         document.getElementById('databoard').innerHTML +=
-            "<tr><td>" + t.team + "</td><td>"
+            "<tr><td>" + t.teamId + "</td><td>"
+            + t.teamName + "</td><td>"
             + t.teamFoundedYear + "</td><td>"
             + t.teamStadiumName + "</td><td>"
-            + t.teamOwner + "</td><td>" + `<button type="button" onclick="Remove(${t.})">` + "</td></tr>";
+            + t.teamOwner + "</td><td>" + `<button type="button" onclick="remove(${t.teamId})"/> Delete` + "</td></tr>";
     });
 }
 
-/*Create func buggy ask Ádám when he gets home*/
+
+
 
 
 function remove(id) {
-    alert(id);
+    fetch('http://localhost:24518/Teams/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); })
 }
 
 
 function create() {
+    let id = document.getElementById('teamid').value;
     let name = document.getElementById('teamname').value;
     let year = document.getElementById('teamfoundedyear').value;
     let stadium = document.getElementById('teamstadiumname').value;
@@ -45,7 +59,7 @@ function create() {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(
-            { teamName: name, teamFoundedYear: year, teamStadiumName: stadium, teamOwner: owner}),
+            { teamId: id, teamName: name, teamFoundedYear: year, teamStadiumName: stadium, teamOwner: owner}),
     })
         .then(response => response)
         .then(data =>
