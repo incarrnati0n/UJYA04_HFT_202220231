68a338c80c747c15ed7c4332bc98d8771a4c48a9
incarrnati0n <horvathimi2001@gmail.com>
2023-03-17 17:22:28 +0100

    Added signalR

--- UJYA04_HFT_202220232.JSClient/wwwroot/TeamsScript.js
+++ UJYA04_HFT_202220232.JSClient/wwwroot/TeamsScript.js
@@ -1,6 +1,32 @@
 ﻿let teams = [];
-
+let connection;
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:24518/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("TeamsCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("TeamsDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+            await start();
+    });
+    start();
+
+
+}
+
+
+
 
 async function getdata() {
     await fetch('http://localhost:24518/Teams/')
@@ -13,7 +39,15 @@ async function getdata() {
 }
 
 
-
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
 
 
 function display() {
