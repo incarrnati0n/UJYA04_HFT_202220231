0820706949822d21ddc5fbe329c99434162837f8
incarrnati0n <horvathimi2001@gmail.com>
2023-04-29 15:05:16 +0200

    NonCrud.html created and done, css for the main table done, working on js functions

--- UJYA04_HFT_202220232.JSClient/wwwroot/NonCrudScript.js
+++ UJYA04_HFT_202220232.JSClient/wwwroot/NonCrudScript.js
@@ -1,1 +1,58 @@
-﻿
+﻿let manager;
+let managername;
+let player;
+
+function getManager() {
+    let id = document.getElementById("shnum_input");
+    fetch(`http://localhost:24518/stat/ListPlayerByShirtNumber/${id.value}`)
+        .then(x => x.json())
+        .then(y => {
+            manager = y;
+            console.log(manager);
+            managerDisplay();
+        })
+}
+
+
+function getManagerName() {
+    let id = document.getElementById("teamid_input");
+    fetch(`http://localhost:24518/stat/ManagerName/${id.value}`)
+        .then(x => x.json())
+        .then(y => {
+            managername = y;
+            console.log(managername);
+            document.getElementById('resultdiv').innerHTML =
+                "<label>The manager's name is: " + managername + "</label>";
+            document.getElementById('resultdiv').style.display = 'flex';
+        })
+}
+
+function highestRated() {
+    let age = document.getElementById("age_input");
+    let teamname = document.getElementById("teamname_input");
+    fetch(`http://localhost:24518/stat/HighestRatingByTeamAndAge/${age},${teamname}`)
+        .then(x => x.json())
+        .then(y => {
+            playername = y;
+            console.log(playername);
+            playerDisplay();
+        })
+}
+
+function managerDisplay() {
+    document.getElementById('resultdiv').innerHTML = "";
+    manager.forEach(t => {
+        document.getElementById('resultdiv').innerHTML +=
+            "<tr><td>" + "The queried manager's name: " + t.managerName  + "</td><td>"
+            + "</td></tr>";
+    });
+}
+
+function playerDisplay() {
+    document.getElementById('resultdiv').innerHTML = "";
+    player.forEach(t => {
+        document.getElementById('resultdiv').innerHTML +=
+            "<tr><td>" + "The queried player's name: " + t.playername + " rating: " +  t.rating + "</td><td>"
+            + "</td></tr>";
+    });
+}
